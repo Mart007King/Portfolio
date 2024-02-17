0fb2d6ea6461f74b69b5d037aa79bc8dd5880b04
mart007king <martinssam17@gmail.com>
2024-02-17 19:04:52 +0100

    all upgrades done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import brand from './bio_images/brand.png';
 import code from './bio_images/code.jpg';
 import soft from './bio_images/soft.jpg';
 import cv from './sites/CV.pdf';
+import fb from './icon/facebook.png';
+import linked from './icon/linked.png';
+import ig from './icon/instagram.png';
+import x from './icon/x.png';
+import git from './icon/github.png';
+import zwork from './bio_images/zwork.jpg';
 import { Reveal } from './component/Reveal';
 
 import './App.css';
@@ -137,13 +143,13 @@ function App() {
       <Reveal>
         <div className="row">
 
-            <div className="col-md text2">
+            <div className="col-md-12 col-sm-12 text2">
 
-                <p className="text m-3">
+                <p className="text card card-body m-3">
               
-                  Hello there! Welcome to my world. A place where 
-                  businesses and humans align with my juice which allows
-                their vision fly. A place where all tech related issues has a solution.
+                  <span className="text-center text-lg"> <b>Hello there! I'm Martins. A PHP DEVELOPER.</b></span>  <p> Welcome to my world. A place where 
+                  businesses and humans align with my solutions which allows
+                their vision fly. A place where all tech related issues has a solution.</p>
                 </p>
             </div>
 
@@ -167,69 +173,77 @@ function App() {
       </Reveal>
     </div>
     <div className="container-fluid">
-      <div className="row">
-        <Reveal>
-          <div className="col-md-12 box1">
-            <div className="card" style={{ width: "30rem" }}>
-              <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
-                <img
-                  src={brand}
-                  className="card-img-top img-fluid"
-                  alt="pic for branding"
-                />
-                <div className="card-body">
-                  <p className="card-text text-info">
-                    I specialize in making a brand for a company which includes
-                    creation of a unique logo, brand font and official color.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Reveal>
-      </div>
-      <div className="row">
-        <Reveal>
-          <div className="col-md-12 box2">
-            <div className="card" style={{ width: "30rem" }}>
-              <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
-                <img
-                  src={code}
-                  className="card-img-top img-fluid"
-                  alt="pic for web dev"
-                />
-                <div className="card-body">
-                  <p className="card-text text-info">
-                    And then proceed to to design the website layout that
-                    complements the brand name, logo and color.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Reveal>
-      </div>
-      <div className="row">
-        <Reveal>
-          <div className="col-md-12 box3">
-            <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+      <Reveal>
+        <div className="row">
+          
+            <div className="col-md-12 box1">
               <div className="card" style={{ width: "30rem" }}>
-                <img
-                  src={soft}
-                  className="card-img-top img-fluid"
-                  alt="pic for software dev"
-                />
-                <div className="card-body">
-                  <p className="card-text text-info">
-                    Website applications that involves both the back-end and
-                    front-end of the development will all be taken care of by me.
-                  </p>
+                <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+                  <img
+                    src={brand}
+                    className="card-img-top img-fluid"
+                    alt="pic for branding"
+                  />
+                  <div className="card-body">
+                    <p className="card-text text-info">
+                      I specialize in making a brand for a company which includes
+                      creation of a unique logo, brand font and official color.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </Reveal>
-      </div>
+          
+        </div>
+      </Reveal>
+
+      <Reveal>
+        <div className="row mt-3">
+          
+            <div className="col-md-12 box2">
+              <div className="card" style={{ width: "30rem" }}>
+                <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+                  <img
+                    src={code}
+                    className="card-img-top img-fluid"
+                    alt="pic for web dev"
+                  />
+                  <div className="card-body">
+                    <p className="card-text text-info">
+                      And then I proceed to gathering necessary requirements for your web app and design the website layout that
+                      complements the brand name, logo and color.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          
+        </div>
+      </Reveal>
+
+      <Reveal>
+        <div className="row mt-3">
+          
+            <div className="col-md-12 box3 ">
+              <div className="card" style={{ width: "30rem" }}>
+                <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+                  <img
+                    src={soft}
+                    className="card-img-top img-fluid"
+                    alt="pic for software dev"
+                  />
+                  <div className="card-body">
+                    <p className="card-text text-info">
+                      Website applications that involves both the back-end and
+                      front-end of the development will all be taken care by me.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          
+        </div>
+      </Reveal>
 
       <Reveal>
         <div className="row m-5">
@@ -244,58 +258,81 @@ function App() {
                 }}
               >
                 <i className="fa-solid fa-martini-glass-citrus" />
-                <span style={{ fontSize: 17 }}>Recent Sites My Juice Designed</span>
+                <span style={{ fontSize: 17 }}>Recent Projects</span>
               </div>
             
           </div>
         </div>
       </Reveal>
     </div>
-    <div className="container">
-      <div className="row">
-        <div className="col-md">
-          <div className="row">
-            <div className="col-sm-6 mb-3 mb-sm-0">
-              <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
-                <Reveal>
-                  <div className="card">
-                    <div className="card-body">
-                      <div className="ratio ratio-16x9">
-                        <iframe src='#' allowFullScreen="" />
+    <div className="container"> 
+      <div className="row ">
+        <div className="col-md ">
+          
+            <div className="row d-flex justify-content-center">
+              <div className="col-sm-6 mb-3 mb-sm-0 ">
+                <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+                  <Reveal>
+                    
+                      <div className="card">
+                        <div className="card-body">
+                          <div className="ratio ratio-16x9">
+                            <img src={zwork} alt="sites done" />
+                          </div>
+
+                          <div className="card flex-column p-2 mt-2">
+
+                            <p className="text-dark"> <b>About Project:</b> This is a freelance marketplace whee freelancers can advertise their skills and get paid jobs by clients who want them to complete any particular job.</p>
+
+                            <p className="text-dark">
+                              <b> Technology used: </b> 
+                              <span className='badge text-bg-secondary'>Object-Oriented Programming (OOP)</span> &nbsp;
+                              <span className='badge text-bg-secondary'>PHP</span> &nbsp;
+                              <span className='badge text-bg-secondary'>AJAX</span> &nbsp;
+                              <span className='badge text-bg-secondary'> J-query </span> &nbsp;
+                              <span className='badge text-bg-secondary'> Javascript </span> &nbsp;
+
+                            </p>
+
+                            <p className="text-dark">
+
+                              <b>Features:</b> 
+                              <ul>
+                                <li>Job creation</li>
+                                <li>In-built Chat feature</li>
+                                <li>Payment Integration (Paystack Api)</li>
+                              </ul>
+
+
+                            </p>
+
+                          </div>
+
+                          <div className="d-flex p-2 mt-2 gap-2">
+
+                            <a href="https://github.com/Mart007King" target="_blank" className="btn btn-sm btn-secondary">
+                              
+                              <img src={git} width="10%" /> &nbsp;
+                              View on GitHub
+                            </a>
+
+                            <a href="https://github.com/Mart007King" target="_blank" className="btn btn-sm btn-primary">
+                              <img src={linked} width="10%" /> &nbsp;
+                              View on LinkedIn
+                            </a>
+
+                          </div>
+                          
+                          
+                        </div>
                       </div>
-                      <a
-                        href="index.html"
-                        className="btn m-3"
-                        style={{ backgroundColor: "#D14081 !important" }}
-                      >
-                        Visit Site
-                      </a>
-                    </div>
-                  </div>
-                </Reveal>
+                    
+                  </Reveal>
+                </div>
               </div>
+              
             </div>
-            <div className="col-sm-6">
-              <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
-                <Reveal>
-                  <div className="card">
-                    <div className="card-body">
-                      <div className="ratio ratio-16x9">
-                        <iframe src='#' allowFullScreen="" />
-                      </div>
-                      <a
-                        href="gas_up"
-                        className="btn m-3"
-                        style={{ backgroundColor: "#D14081 !important" }}
-                      >
-                        Visit Site
-                      </a>
-                    </div>
-                  </div>
-                </Reveal>
-              </div>
-            </div>
-          </div>
+      
         </div>
       </div>
     </div>
@@ -306,32 +343,20 @@ function App() {
     </div>
     <div className="container-fluid footer">
       <div className="row">
-        <div className="col-md my-5">
-          <div className="card text-center">
-            <div className="card-header"></div>
-            <Reveal>
-              <div className="card-body">
-                <div className="btn-group">
-                  <button
-                    className="btn btn-secondary btn-lg dropdown-toggle"
-                    type="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    About Me
-                  </button>
-                  <div
-                    className="dropdown-menu p-4 text-body-info drop"
-                    style={{ maxWidth: 200 }}
-                  >
-                    <a href={cv} target="_blank">
-                      Download Cv
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </Reveal>
-          </div>
+        <div className="col-md my-5 d-flex justify-content-center">
+
+            <div className="dropdown">
+              <button className="btn btn-secondary dropdown-toggle btn-lg" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                About Me
+              </button>
+              <ul className="dropdown-menu text-body-info drop">
+                <a href={cv} target="_blank">
+                  Download Cv
+                </a>
+                
+              </ul>
+            </div>
+      
         </div>
         <div className="row">
           <div className="col-md my-5">
@@ -339,25 +364,44 @@ function App() {
               <div className="card text-center">
                 <div className="card-header"></div>
                 <div className="card-body">
-                  <h5 className="card-title">FIND ME EVERYWHERE</h5>
+                  <h5 className="card-title mb-3">FIND ME EVERYWHERE</h5>
+
                   <p className="card-text">
-                    <a href="#" className="btn btn-primary">
-                      <img src="icon/facebook.png" width="10%" />
+                    <a href="https://github.com/Mart007King" target="_blank" className="btn btn-primary">
+                      <img src={git} width="10%" />
                     </a>
                   </p>
                   <br />
+
                   <p className="card-text">
-                    <a href="#" className="btn btn-primary">
-                      <img src="icon/instagram.png" width="10%" />
+                    <a href="https://www.linkedin.com/in/martins-samuel-a07495187/" target="_blank" className="btn btn-primary">
+                      <img src={linked} width="10%" className="img-fluid" />
                     </a>
                   </p>
                   <br />
+
+
                   <p className="card-text">
-                    <a href="#" className="btn btn-primary">
-                      <img src="icon/twitter.png" width="10%" />
+                    <a href="https://twitter.com/King_mhartins" target="_blank" className="btn btn-primary">
+                      <img src={x} width="10%" />
                     </a>
                   </p>
                   <br />
+
+                  <p className="card-text">
+                    <a href="https://www.facebook.com/martins.samuel.52" target="_blank" className="btn btn-primary">
+                      <img src={fb} width="10%" />
+                    </a>
+                  </p>
+                  <br />
+                  
+                  <p className="card-text">
+                    <a href="https://www.instagram.com/ma_rtins_/?hl=en" target="_blank" className="btn btn-primary">
+                      <img src={ig} width="10%" />
+                    </a>
+                  </p>
+                  <br />
+                  
                 </div>
               </div>
             </Reveal>
